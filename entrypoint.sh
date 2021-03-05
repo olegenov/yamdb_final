9b77053ac058eab0f1db52f96a02b7043d0626e1
olegenov <nik_kitaev@mail.ru>
2021-03-05 14:11:05 +0300

    nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createcachetable
 python manage.py collectstatic  --noinput
-gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
+gunicorn yamdb_final.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
